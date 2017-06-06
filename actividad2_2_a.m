e7d8122e7b142a6e7d79b4d287679822802afccf
Ignacio Ibañez <ignacio.ibanez@usach.cl>
2017-06-06 16:33:18 -0400

    parte 2 terminada falta revision

---
+++ actividad2_2_a.m
@@ -0,0 +1,48 @@
+function actividad2_2_a(r1, r2, a1, a2)
+
+%parametros
+%r1: llave de paso del F1
+%r2: llava de paso del F2
+%a1: area basal del recipiente 1
+%a2: area basal del recipiente 2
+
+%entrada 
+%F0: flujo de entrada en el estanque
+
+%estado
+%l1: altura del flujo en el recipiente 1
+%l2: altura del flujo en el recipiente 2
+
+%salida
+%l1: altura del flujo en el recipiente 1
+%l2: altura del flujo en el recipiente 2
+
+%X' =  Ax  +  Bu
+%Y  =  Cx  +  Du
+%modelo
+A = [(-1/(r1*a1)) (1/(a1*r1));(1/(a2*r1)) ((-1/(a2*r1))-(1/(a2*r2)))];
+B = [1/a1; 0];
+C = [1 0; 0 1];
+D = [0;0];
+
+%[num, den] = ss2tf(A,B,C,D);
+%H1 = tf(num(1,:), den);
+%H2 = tf(num(2,:), den);
+%[y,t] = step(H1);
+%plot(t,y)
+
+syms s
+%Del modelo obtener funciones de transferencia
+modelo = C*inv(s*eye(2)-A)*B+D;
+t = 0:0.1:100;
+
+%Son evaluadas cada una de las funciones de transferencia
+y1 = subs(ilaplace(modelo(1)*(1/s)),t);
+y2 = subs(ilaplace(modelo(2)*(1/s)),t);
+
+%graficar funcion de transferencia 1
+createfigure1(t,y1,'Respuesta impulso unitario funcion de transferencia l1', 1);
+pause;
+%graficar funcion de transferencia 1
+createfigure1(t,y2,'Respuesta impulso unitario funcion de transferencia l2', 1);
+pause;
---
+++ actividad2_2_b.m
@@ -0,0 +1,54 @@
+function actividad2_2_b(r1, r2, c1, c2)
+
+%parametros
+%r1: resistencia 1
+%r2: resistencia 2
+%c1: capacitancia del condensador 1
+%c2: capacitancia del condensador 2
+
+%entrada 
+%Vs voltaje de la fuente
+
+%estado
+%Vc1: voltaje del condensador 1
+%Vc2: voltaje del condensador 2
+
+%salida
+%Vc1: voltaje del condensador 1
+%Vc2: voltaje del condensador 2
+
+%parametros
+%r1
+%r2
+%a1
+%a2
+
+%entrada F0
+
+%estado
+%l1 y l2
+
+%X' =  Ax  +  Bu
+%Y  =  Cx  +  Du
+%modelo
+A = [(-1/(r1*c1)) 0;0 (-1/(r2*c2))];
+B = [(1/(c1*r1)); (1/(c2*r2))];
+C = [1 0; 0 1];
+D = [0;0];
+
+syms s
+%Del modelo obtener funciones de transferencia
+modelo = C*inv(s*eye(2)-A)*B+D;
+t = 0:0.1:100;
+
+%Son evaluadas cada una de las funciones de transferencia
+y1 = subs(ilaplace(modelo(1)*(1/s)),t);
+y2 = subs(ilaplace(modelo(2)*(1/s)),t);
+
+%graficar funcion de transferencia 1
+createfigure1(t,y1,'Respuesta impulso unitario funcion de transferencia Vc1', 1);
+pause;
+%graficar funcion de transferencia 1
+createfigure1(t,y2,'Respuesta impulso unitario funcion de transferencia Vc2', 1);
+pause;
+
